e924611d355dce67e1bce2ef7927149e6064d323
Marcos Tullio Silva de Souza <marcostulilo.s@gmail.com>
2026-02-25 19:03:22 -0300

    fix: init

--- examples/apontar_um_serial.py
+++ examples/apontar_um_serial.py
@@ -1,4 +1,5 @@
 from pycmcontrol_mqtt import CmControlClient, CmControlConfig
+from pycmcontrol_mqtt.errors import CmcConnectionError, CmcLoginError, CmcApontamentoError, CmcTimeout
 
 cfg = CmControlConfig(
     device_addr="device001",
@@ -10,7 +11,15 @@ cfg = CmControlConfig(
     api_pass="senha",
 )
 
-with CmControlClient(cfg) as cmc:
-    cmc.ensure_login()
-    resp = cmc.apontar_serial("00000203030300")
-    print(resp)
+try:
+    with CmControlClient(cfg) as cmc:
+        cmc.ensure_login()
+        cmc.apontar_serial("00000203030300")
+except CmcConnectionError as e:
+    print("Falha broker:", e)
+except CmcLoginError as e:
+    print("Falha login:", e)
+except CmcApontamentoError as e:
+    print("Falha apontamento:", e.status, e.log)
+except CmcTimeout as e:
+    print("Timeout:", e)
--- src/pycmcontrol_mqtt/__init__.py
+++ src/pycmcontrol_mqtt/__init__.py
@@ -1,19 +1,64 @@
+"""
+pycmcontrol-mqtt
+
+Cliente MQTT para Driver CmControl v1.00 (MQTT + MQTT+REST + OAuth2)
+"""
+
 from .config import CmControlConfig
-from .client import CmControlClient
-from .errors import CmcError, CmcTimeout, CmcNotConnected, CmcLoginError, CmcRequestError
-from .models import SetupApontamento, Apontamento, Serial, OrdemTransporte, Evidence
+from .client import CmControlClient, BrokerTLS
+
+from .models import (
+    SetupApontamento,
+    Apontamento,
+    Serial,
+    OrdemTransporte,
+    Evidence,
+)
+
+from .errors import (
+    CmcError,
+    CmcConfigError,
+    CmcNotConnected,
+    CmcInvalidArgument,
+    CmcConnectionError,
+    CmcConnectionTimeout,
+    CmcDnsError,
+    CmcTlsError,
+    CmcMqttProtocolError,
+    CmcMqttAuthError,
+    CmcDisconnected,
+    CmcTimeout,
+    CmcDecodeError,
+    CmcLoginError,
+    CmcApiError,
+    CmcApontamentoError,
+    CmcResponseError,
+)
 
 __all__ = [
-    "CmControlConfig",
     "CmControlClient",
-    "CmcError",
-    "CmcTimeout",
-    "CmcNotConnected",
-    "CmcLoginError",
-    "CmcRequestError",
+    "BrokerTLS",
+    "CmControlConfig",
     "SetupApontamento",
     "Apontamento",
     "Serial",
     "OrdemTransporte",
     "Evidence",
+    "CmcError",
+    "CmcConfigError",
+    "CmcNotConnected",
+    "CmcInvalidArgument",
+    "CmcConnectionError",
+    "CmcConnectionTimeout",
+    "CmcDnsError",
+    "CmcTlsError",
+    "CmcMqttProtocolError",
+    "CmcMqttAuthError",
+    "CmcDisconnected",
+    "CmcTimeout",
+    "CmcDecodeError",
+    "CmcLoginError",
+    "CmcApiError",
+    "CmcApontamentoError",
+    "CmcResponseError",
 ]
