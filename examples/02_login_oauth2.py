98ec43ac57c953f608444de4b1cac0327b9e1764
Marcos Tullio Silva de Souza <marcostulilo.s@gmail.com>
2026-02-26 09:52:06 -0300

    fix: ex02 corrected

--- examples/02_login_oauth2.py
+++ examples/02_login_oauth2.py
@@ -9,7 +9,7 @@ cfg = cfg_from_env_or_defaults()
 with CmControlClient(cfg, debug=True) as cmc:
     try:
         tok = cmc.login_oauth2()
-        print("Token obtido:", tok[:24] + "...")
+        print("Token obtido:", tok[:12] + "...")
         print("Token válido?", cmc.is_token_valid())
     except CmcLoginError as e:
         print("Falha login:", e.status, e.log)
